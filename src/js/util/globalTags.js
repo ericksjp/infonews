d96bceb2e796ecf2fd86178b3e2e817abdbc9338
ericksjp <ericksjp703@gmail.com>
2025-01-20 22:30:10 -0300

    feat: Implementa header reutilizável com menu de navegação
    - Atualiza `index.html` com um header estruturado contendo o logotipo e o menu
      de navegação.
    - Cria `globalListeners.js` para gerenciar interações do header (header fixo,
      alternância do menu hamburger e interações com os itens do menu).
    - Adiciona `globalTags.js` para centralizar referências dos elementos DOM
      usados nas páginas.
    - Atualiza `styles.css` com o estilo do header, design do menu de navegação
      e consultas de mídia para responsividade.
    - Atualiza `home.js` para importar os listeners globais.
    
    closes issue #1

---
+++ src/js/util/globalTags.js
@@ -0,0 +1,5 @@
+// Esse arquivo contem as tags que são usadas em todas as páginas do site.
+export const header = document.querySelector("header");
+export const title = document.querySelector("#ifn-title");
+export const hamburger = document.querySelector(".hamburger");
+export const nav_menu = document.querySelector(".nav-menu");
