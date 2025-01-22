1a926f7e6cde87c6fb4415906ae14f372fa0db42
ericksjp <ericksjp703@gmail.com>
2025-01-22 19:09:33 -0300

    feat: Implementa visualização dinâmica de notícias na Homepage
    - Adiciona `homeUiHandler.js` e `generic.js` para renderização dinâmica das
      notícias na homepage.
    - Inclui estilizações em `home.css` e `styles.css`.
    - Refatora `index.html` para remover valores hard coded.

--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,7 @@
+import { adicionarNoticias } from "./ui/homeUiHandler.js";
 import "./listeners/globalListeners.js";
+import noticias from "./util/mockData.js";
+
+(() => {
+  adicionarNoticias(noticias.all.articles);
+})();
