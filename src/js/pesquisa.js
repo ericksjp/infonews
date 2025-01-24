d176b28ea4c1e4b673e6b41a4125ba20d282056c
Diego Sousa <diego7lts7@gmail.com>
2025-01-24 09:12:03 -0300

    refactor and feat: ajustando detalhes no card pesquisa e implementando o evento de abrir o modal noticia a partir do card da noticia na pesquisa.

--- src/js/pesquisa.js
+++ src/js/pesquisa.js
@@ -1,41 +1,57 @@
-const API_KEY = 'chave_da_api';
+import { modalNews } from "./util/globalTags";
+
+const API_KEY = "23df153f2c7b4529bc4cd27f5d701952";
 
 const fetchNews = (query) => {
-    fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`)
-        .then(response => response.json())
-        .then(data => {
-            const newsResults = document.getElementById('newsResults');
-            newsResults.innerHTML = '';
-            if (data.articles.length === 0) {
-                newsResults.innerHTML = '<p id="no-results-message">Nenhum resultado encontrado.</p>';
-            } else {
-                data.articles.forEach(article => {
-                    const articleElement = document.createElement('div');
-                    articleElement.innerHTML = `
-                        <img src="${article.urlToImage || 'placeholder.jpg'}" alt="News Image">
+  fetch(`https://newsapi.org/v2/everything?q=${query}&apiKey=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => {
+      const newsResults = document.getElementById("newsResults");
+      newsResults.innerHTML = "";
+      if (data.articles.length === 0) {
+        newsResults.innerHTML =
+          '<p id="no-results-message">Nenhum resultado encontrado.</p>';
+      } else {
+        data.articles.forEach((article) => {
+          const articleElement = document.createElement("div");
+          articleElement.innerHTML = `
+                        <img src="${
+                          article.urlToImage || "placeholder.jpg"
+                        }" alt="News Image">
                         <h2>${article.title}</h2>
                         <p>${article.description}</p>
-                        <span>Por ${article.author || 'Desconhecido'} em ${new Date(article.publishedAt).toLocaleDateString()}</span>
-                        <a href="${article.url}" target="_blank">Leia mais</a>
+                        <span>Por ${
+                          article.author || "Desconhecido"
+                        } em ${new Date(
+            article.publishedAt
+          ).toLocaleDateString()}</span>
+                        <a>Leia mais</a>
                     `;
-                    newsResults.appendChild(articleElement);
-                });
-            }
-        })
-        .catch(error => {
-            console.error('Erro ao buscar notícias:', error);
-            const newsResults = document.getElementById('newsResults');
-            newsResults.innerHTML = '<p id="no-results-message">Ocorreu um erro ao buscar as notícias. Tente novamente mais tarde.</p>';
+
+          articleElement.addEventListener("click", () =>
+            modalNews.create(article)
+          );
+          newsResults.appendChild(articleElement);
         });
+      }
+    })
+    .catch((error) => {
+      console.error("Erro ao buscar notícias:", error);
+      const newsResults = document.getElementById("newsResults");
+      newsResults.innerHTML =
+        '<p id="no-results-message">Ocorreu um erro ao buscar as notícias. Tente novamente mais tarde.</p>';
+    });
 };
 
-document.getElementById('searchForm').addEventListener('submit', function(event) {
+document
+  .getElementById("searchForm")
+  .addEventListener("submit", function (event) {
     event.preventDefault();
-    const query = document.getElementById('searchInput').value;
+    const query = document.getElementById("searchInput").value;
     fetchNews(query);
-});
+  });
 
-const initialQuery = localStorage.getItem('searchQuery');
+const initialQuery = localStorage.getItem("searchQuery");
 if (initialQuery) {
-    fetchNews(initialQuery);
+  fetchNews(initialQuery);
 }
